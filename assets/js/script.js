0cc7f592ed9511bf9d37637c9871e4219c8419e1
Bdoherty1 <bdoherty988@gmail.com>
2024-01-16 13:16:40 -0600

    index update, tailwind styling, and formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-const jokeText = document.querySelector('.joke-text');
-// .new-joke-btn 
-const randomJoke = document.querySelector('.randombtn');
-// .save-btn ?(link)?//
-const saveBtn = document.querySelector('.saveBtn');
+// const jokeText = document.querySelector('.joke-text');
+// // .new-joke-btn 
+// const randomJoke = document.querySelector('.randombtn');
+// // .save-btn ?(link)?//
+// const saveBtn = document.querySelector('.saveBtn');
 
-// randomJoke.addEventListener('click', getJoke);
-// getJoke();
+// // randomJoke.addEventListener('click', getJoke);
+// // getJoke();
